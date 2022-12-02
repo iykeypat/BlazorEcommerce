6810d75e0a6e5f715716a2267a473f7453bfdb99
ik ikey <patrickiykey@gmail.com>
2022-12-02 13:52:37 +0100

    Add and authenication service on the server

---
+++ BlazorEcommerce/Server/Services/AuthService/AuthService.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorEcommerce.Server.Services.AuthService
+{
+    public class AuthService : IAuthService
+    {
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
