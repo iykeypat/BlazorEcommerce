dd92f61beea8c807341ea986d931f7a7921cecad
ik ikey <patrickiykey@gmail.com>
2022-12-02 14:10:39 +0100

    Check if a user is alredy existing

--- BlazorEcommerce/Server/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Server/Services/AuthService/AuthService.cs
@@ -2,14 +2,30 @@
 {
     public class AuthService : IAuthService
     {
+        DataContext _context;
+        public AuthService(DataContext context)
+        {
+            _context = context;
+        }
+
+        //for user registration
         public Task<ServiceResponse<int>> Register(User user, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> UserExists(string email)
+
+       //Checks if the uder already exist
+        public async Task<bool> UserExists(string email)
         {
-            throw new NotImplementedException();
+            if (await _context.Users.AnyAsync( user => user.Email.ToLower().Equals(email.ToLower()) ))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
