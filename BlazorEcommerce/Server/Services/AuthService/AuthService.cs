05f5cdb1e7c88348c3bfe8d7b5994cd90394ddbe
ik ikey <patrickiykey@gmail.com>
2022-12-08 09:05:27 +0100

    Implement user registeration on the server

--- BlazorEcommerce/Server/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Server/Services/AuthService/AuthService.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorEcommerce.Server.Services.AuthService
+﻿using System.Security.Cryptography;
+
+namespace BlazorEcommerce.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
@@ -9,13 +11,29 @@
         }
 
         //for user registration
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            if (await UserExists(user.Email))
+            {
+                return new ServiceResponse<int> { Success = false, Message = "User already exists." };
+            }
+
+            CreatePasswordHash(password,out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int>()
+            {
+                Data = user.Id
+            };
         }
 
 
-       //Checks if the uder already exist
+       //Checks if the user already exist
         public async Task<bool> UserExists(string email)
         {
             if (await _context.Users.AnyAsync( user => user.Email.ToLower().Equals(email.ToLower()) ))
@@ -25,6 +43,16 @@
             else
             {
                 return false;
+            }
+        }
+
+        //Create password hash
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
     }
