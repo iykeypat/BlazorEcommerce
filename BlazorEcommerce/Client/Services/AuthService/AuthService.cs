7dabb865398619de2abd5c456414f4d49df52f4b
ik ikey <patrickiykey@gmail.com>
2022-12-08 10:55:11 +0100

    Implement the registration on the client

--- BlazorEcommerce/Client/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Client/Services/AuthService/AuthService.cs
@@ -2,5 +2,18 @@
 {
     public class AuthService : IAuthService
     {
+        private readonly HttpClient _client;
+
+        public AuthService(HttpClient httpClient)
+        {
+            _client = httpClient;
+        }
+
+        public async Task<ServiceResponse<int>> Register(UserRegister request)
+        {
+            var result = await _client.PostAsJsonAsync("api/auth/register", request);
+
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
+        }
     }
 }
