1086c79a3923ded208598ec7ef3fa83d4fa7d1d0
ik ikey <patrickiykey@gmail.com>
2022-12-15 19:02:21 +0100

    Implement changing the password on the client

--- BlazorEcommerce/Client/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Client/Services/AuthService/AuthService.cs
@@ -4,11 +4,22 @@
     {
         private readonly HttpClient _client;
 
+
+        //ctor
         public AuthService(HttpClient httpClient)
         {
             _client = httpClient;
         }
 
+        //handles change of password on clientside
+        public async Task<ServiceResponse<bool>> ChangePassword(UserChangePassword request)
+        {
+            var result = await _client.PostAsJsonAsync("api/auth/change-password", request.Password);
+
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
+        }
+
+        //Handles user login on clientside
         public async Task<ServiceResponse<string>> Login(UserLogin request)
         {
             var result = await _client.PostAsJsonAsync("api/auth/login", request);
@@ -16,6 +27,8 @@
             return await result.Content.ReadFromJsonAsync<ServiceResponse<string>>();
         }
 
+
+        //Handles user registration on clientside
         public async Task<ServiceResponse<int>> Register(UserRegister request)
         {
             var result = await _client.PostAsJsonAsync("api/auth/register", request);
