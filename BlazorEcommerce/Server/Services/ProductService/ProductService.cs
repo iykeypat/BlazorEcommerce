da625b13ca69fd20f24ddffbd7df5bb5c8884173
ik ikey <patrickiykey@gmail.com>
2023-01-05 18:16:50 +0100

    Implement the EditProduct Component

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -198,6 +198,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
             dbProduct.ImageUrl= product.ImageUrl;
             dbProduct.CategoryId= product.CategoryId;
             dbProduct.Visible= product.Visible;
+            dbProduct.Featured = product.Featured;
 
             foreach (var variant in product.Variants)
             {
