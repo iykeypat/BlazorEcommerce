a9ab9c19b2553a751dbc1fbe932a35e55ea00d1f
ik ikey <patrickiykey@gmail.com>
2023-01-05 13:17:11 +0100

    Create, Update & Delete products on the server

--- BlazorEcommerce/Server/Controllers/ProductsController.cs
+++ BlazorEcommerce/Server/Controllers/ProductsController.cs
@@ -10,12 +10,13 @@ namespace BlazorEcommerce.Server.Controllers
     {
         private readonly IProductService productService;
       
-
+        //ctor
         public ProductsController(IProductService productService)
         {
             this.productService = productService;
         }
 
+        //This endpoint returns a list of all visible products from DB
         [HttpGet]
         public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
         {
@@ -25,6 +26,7 @@ namespace BlazorEcommerce.Server.Controllers
             return Ok(result); 
         }
 
+        //Returns a single product from DB given the id
         [HttpGet("product/{productId}")]
         public async Task<ActionResult<ServiceResponse<Product>>> GetProductAsync(int productId)
         {
@@ -43,6 +45,7 @@ namespace BlazorEcommerce.Server.Controllers
             return Ok(result);
         }
 
+        //Returns alist of products that belong to a given category
         [HttpGet("category/{categoryUrl}")]
         public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductsByCategory(string categoryUrl)
         {
@@ -52,6 +55,7 @@ namespace BlazorEcommerce.Server.Controllers
             return Ok(result);
         }
 
+        //Returns all products that matches a given search string and paginates the search result
         [HttpGet("search/{searchText}/{page}")]
         public async Task<ActionResult<ServiceResponse<ProductSearchResult>>> SearchProducts(string searchText,int page=1) 
         {
@@ -61,6 +65,7 @@ namespace BlazorEcommerce.Server.Controllers
             return Ok(result);
         }
 
+        //Returns as suggestions, all products whose title or description contains the search string
         [HttpGet("searchsuggestions/{searchText}")]
         public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductSearchSuggestions(string searchText)
         {
@@ -70,6 +75,7 @@ namespace BlazorEcommerce.Server.Controllers
             return Ok(result);
         }
 
+        //Returns a list of products with featured flag set to true
         [HttpGet("featured")]
         public async Task<ActionResult<ServiceResponse<List<Product>>>> GetFeaturedProducts()
         {
@@ -84,6 +90,36 @@ namespace BlazorEcommerce.Server.Controllers
         public async Task<ActionResult<ServiceResponse<List<Product>>>> GetAdminProducts()
         {
             var result = await productService.GetAdminProducts();
+
+
+            return Ok(result);
+        }
+
+        //Creates a new product and returns the newly created product
+        [HttpPost, Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<Product>>> CreateProduct(Product product)
+        {
+            var result = await productService.CreateProduct(product);
+
+
+            return Ok(result);
+        }
+
+        //Updates an existing product and returns it
+        [HttpPut, Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<Product>>> UpdateProduct(Product product)
+        {
+            var result = await productService.UpdateProduct(product);
+
+
+            return Ok(result);
+        }
+
+        //Soft Deletes a product given the product id
+        [HttpDelete("{id}"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<bool>>> DeleteProduct(int id)
+        {
+            var result = await productService.DeleteProduct(id); 
 
 
             return Ok(result);
--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -4,14 +4,44 @@ namespace BlazorEcommerce.Server.Services.ProductService
 {
     public class ProductService : IProductService
     {
-        private readonly DataContext context;
+        private readonly DataContext _context;
         IHttpContextAccessor _httpContextAccessor;
 
         //ctor
         public ProductService(DataContext context, IHttpContextAccessor httpContextAccessor)
         {
-            this.context = context;
+            _context = context;
             _httpContextAccessor = httpContextAccessor;
+        }
+
+        //method for admin to create a new product. it returns the newly created product inside the DTO
+        public async Task<ServiceResponse<Product>> CreateProduct(Product product)
+        {
+            foreach (var variant in product.Variants)
+            {
+                variant.ProductType = null;
+            }
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<Product> { Data= product };
+        }
+
+        //this method allows admin to soft-delete a product
+        public async Task<ServiceResponse<bool>> DeleteProduct(int productId)
+        {
+            var dbProduct = await _context.Products.FindAsync(productId);
+
+            if (dbProduct == null) {
+                return new ServiceResponse<bool> { Success= false, Message = "Product not found. ", Data = false };
+            }
+
+            dbProduct.Deleted= true;
+
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<bool> { Data = true };
         }
 
         //returns all products for admin view whether visible or not
@@ -19,7 +49,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await context.Products.Where(p => !p.Deleted).Include(x => x.Variants.Where(v => !v.Deleted))
+                Data = await _context.Products.Where(p => !p.Deleted).Include(x => x.Variants.Where(v => !v.Deleted))
                 .ThenInclude(v => v.ProductType)
                 .ToListAsync()
             };
@@ -32,7 +62,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await context.Products.Where(p=> p.Featured && p.Visible && !p.Deleted).Include(p => p.Variants.Where(p => p.Visible && !p.Deleted)).ToListAsync()
+                Data = await _context.Products.Where(p=> p.Featured && p.Visible && !p.Deleted).Include(p => p.Variants.Where(p => p.Visible && !p.Deleted)).ToListAsync()
             };
 
             return response;
@@ -44,14 +74,15 @@ namespace BlazorEcommerce.Server.Services.ProductService
             var response = new ServiceResponse<Product>();
 
             Product product = null;
+
             if (_httpContextAccessor.HttpContext.User.IsInRole("Admin"))
             {
-                product = await context.Products.Include(x => x.Variants.Where(v => !v.Deleted))
+                product = await _context.Products.Include(x => x.Variants.Where(v => !v.Deleted))
                 .ThenInclude(y => y.ProductType).FirstOrDefaultAsync(z => z.Id == productId && !z.Deleted);
             }
             else
             {
-                product = await context.Products.Include(x => x.Variants.Where(v => v.Visible && !v.Deleted))
+                product = await _context.Products.Include(x => x.Variants.Where(v => v.Visible && !v.Deleted))
                 .ThenInclude(y => y.ProductType).FirstOrDefaultAsync(z => z.Id == productId && !z.Deleted && z.Visible);
             }
 
@@ -71,8 +102,8 @@ namespace BlazorEcommerce.Server.Services.ProductService
         //returns a single product given the id
         public Product GetProductById(int productId)
         {
-            //return context.Products.First(x=> x.Id == productId);
-            return context.Products.Find(productId);
+            //return _context.Products.First(x=> x.Id == productId);
+            return _context.Products.Find(productId);
         }
 
 
@@ -81,7 +112,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await context.Products.Where(p => p.Visible && !p.Deleted).Include(x => x.Variants.Where(v => v.Visible && !v.Deleted)).ToListAsync()
+                Data = await _context.Products.Where(p => p.Visible && !p.Deleted).Include(x => x.Variants.Where(v => v.Visible && !v.Deleted)).ToListAsync()
             };
 
             return response;
@@ -92,7 +123,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>()
             {
-                Data = await context.Products.Where( x => x.Category.Url.ToLower().Equals(categoryUrl.ToLower()) && x.Visible && !x.Deleted)
+                Data = await _context.Products.Where( x => x.Category.Url.ToLower().Equals(categoryUrl.ToLower()) && x.Visible && !x.Deleted)
                 .Include(p => p.Variants.Where(v => v.Visible && !v.Deleted)).ToListAsync()
             };
 
@@ -136,7 +167,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
             var pageResult = 2f;
             var pageCount = Math.Ceiling((await FindProductsBySearchText(searchText)).Count / pageResult);
 
-            var products = await context.Products.Where(p =>
+            var products = await _context.Products.Where(p =>
                             p.Title.ToLower().Contains(searchText.ToLower()) || p.Description.ToLower().Contains(searchText.ToLower()) && p.Visible && !p.Deleted)
                             .Include(p => p.Variants).Skip((page - 1) * (int)pageResult).Take((int)pageResult).ToListAsync();
 
@@ -153,9 +184,47 @@ namespace BlazorEcommerce.Server.Services.ProductService
             return response;
         }
 
+        public async Task<ServiceResponse<Product>> UpdateProduct(Product product)
+        {
+            var dbProduct = await _context.Products.FindAsync(product.Id);
+
+            if (dbProduct == null)
+            {
+                return new ServiceResponse<Product> { Success = false, Message = "Product not found. " };
+            }
+
+            dbProduct.Title = product.Title;
+            dbProduct.Description = product.Description;
+            dbProduct.ImageUrl= product.ImageUrl;
+            dbProduct.CategoryId= product.CategoryId;
+            dbProduct.Visible= product.Visible;
+
+            foreach (var variant in product.Variants)
+            {
+                var dbVariant = await _context.ProductVariants.SingleOrDefaultAsync(v => v.ProductId == variant.ProductId && v.ProductTypeId == variant.ProductTypeId);
+
+                if (dbVariant == null)
+                {
+                    variant.ProductType = null;
+                    _context.ProductVariants.Add(variant);
+                }
+                else
+                {
+                    dbVariant.ProductTypeId= variant.ProductTypeId;
+                    dbVariant.Price= variant.Price;
+                    dbVariant.OriginalPrice= variant.OriginalPrice;
+                    dbVariant.Visible = variant.Visible;
+                    dbVariant.Deleted= variant.Deleted;
+                }
+            }
+
+            await _context.SaveChangesAsync();
+            return new ServiceResponse<Product> { Data = product };
+        }
+
         private async Task<List<Product>> FindProductsBySearchText(string searchText)
         {
-            return await context.Products.Where(p =>
+            return await _context.Products.Where(p =>
                             p.Title.ToLower().Contains(searchText.ToLower()) || p.Description.ToLower().Contains(searchText.ToLower()) && p.Visible && !p.Deleted)
                             .Include(p => p.Variants.Where(p => p.Visible && !p.Deleted)).ToListAsync();
         }
